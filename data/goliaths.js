ddf340093a239518f7cb1dd28aba510af5402542
jpfraneto <jpfraneto@gmail.com>
2023-03-05 12:32:13 -0300

    added goliaths

---
+++ data/goliaths.js
@@ -0,0 +1,54 @@
+export let goliaths = [
+  {
+    number: 2075,
+    ownedBy: '@kithkui',
+    imageUrl:
+      'https://i.seadn.io/gcs/files/def400e3aa3d98cb0536a65b691ac20b.gif',
+    musicInfo: {
+      cover:
+        'https://i.discogs.com/007Z7J1c4ow3elf_ddnXCJPL2g6EGbya4TJZYBzqs_0/rs:fit/g:sm/q:90/h:600/w:598/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzNDc2/OTExLTE1NTQ5NDIw/MjctNjk2Ni5qcGVn.jpeg',
+      title: 'Henosis',
+      author: 'Joep Beving',
+      year: 2019,
+    },
+  },
+  {
+    number: 2077,
+    ownedBy: '@kithkui',
+    imageUrl:
+      'https://i.seadn.io/gcs/files/5137838337952a371ec5f1273e5871bc.png',
+    musicInfo: {
+      cover:
+        'https://i.discogs.com/u7VRwgvIuEMZvsgu7L9BX8FoeEcREusIXpbxMQUNk3k/rs:fit/g:sm/q:90/h:509/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1Nzk0/MDk5LTE1OTc5MTI0/NzAtNzIxNy5qcGVn.jpeg',
+      title: 'Henosis',
+      author: 'Joep Beving',
+      year: 2019,
+    },
+  },
+  {
+    number: 2070,
+    ownedBy: '@kithkui',
+    imageUrl:
+      'https://i.seadn.io/gcs/files/5ec8d3ba599815d609903d643dc8fb42.gif',
+    musicInfo: {
+      cover:
+        'https://i.discogs.com/JQrO2sz84IyUKNp6S2XqPDiO2LBPeHzAkEaYncGr4dg/rs:fit/g:sm/q:90/h:557/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTYzNzM2/Mi0xNDczMTgxOTIz/LTExMDEuanBlZw.jpeg',
+      title: 'Henosis',
+      author: 'Joep Beving',
+      year: 2019,
+    },
+  },
+  {
+    number: 5,
+    ownedBy: '@kithkui',
+    imageUrl:
+      'https://i.seadn.io/gcs/files/6da8d33d20ec68970401b75eaa5f9f78.png',
+    musicInfo: {
+      cover:
+        'https://i.discogs.com/ag9EY1BSTcY2TCwvDHJO6V-ynkQ7v2acCe1KGR5vNEA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1Nzk1/NTYwLTE1OTc5Mzcy/ODgtMTgyNi5qcGVn.jpeg',
+      title: 'Source',
+      author: 'Nubya Garcia',
+      year: 2020,
+    },
+  },
+];
